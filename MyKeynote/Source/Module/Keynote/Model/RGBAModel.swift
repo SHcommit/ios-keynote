a9a2d4e4679dd1674d09f7b460a09839e7838606
shcommit <happysh_s2@naver.com>
2023-07-20 00:06:36 +0900

    [Feat] RGBA model 구현

---
+++ MyKeynote/Source/Module/Keynote/Model/RGBAModel.swift
@@ -0,0 +1,30 @@
+//
+//  RGBAModel.swift
+//  MyKeynote
+//
+//  Created by 양승현 on 2023/07/19.
+//
+
+import Foundation
+
+struct RGBAModel {
+  @GrayscaleRange var red: Int
+  @GrayscaleRange var green: Int
+  @GrayscaleRange var blue: Int
+  var alpha: AlphaModel
+}
+
+// MARK: - Properties
+extension RGBAModel {
+  mutating func isValidAlphaRange(_ target: UInt8) -> Bool {
+    return alpha.isValidRange(target)
+  }
+  
+  mutating func upAlhpaValue() {
+    alpha.upAlphaValue()
+  }
+  
+  mutating func downAlphaValue() {
+    alpha.downAlphaValue()
+  }
+}
