9053ed74be1c546833d0afa9230cfed688d3fed9
shcommit <happysh_s2@naver.com>
2023-07-22 20:02:08 +0900

    [Docs] LayoutSupprot usage 추가!!

--- MyKeynote/Source/Common/Protocol/LayoutSupport.swift
+++ MyKeynote/Source/Common/Protocol/LayoutSupport.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
+
 /// UIView's layout support
+///
+///  Notes:
+/// 1. UIView의 레이아웃을 지정할 때 사용되는 프로토콜입니다.
+///
+///  Examples:
+/// ```
+/// class ViewController: UIViewController {
+///   // MARK: - Properties
+///   let emptyView = {
+///     let emptyView = UIView()
+///     ...
+///     return emptyView
+///   }()
+///   let aView = UIView()
+///   let bView = UIView()
+///
+///   // MARK: - Lifecycle
+///   override func viewDidLoad() {
+///     super.viewDidLoad()
+///     setupUI(emptyView, aView, bView)
+///   }
+/// }
+///
+/// // MARK: - LayoutSupport
+/// extension ViewController: LayoutSupport {
+///
+///  func setConstraints() {
+///    //이곳에서 뷰에 대한 constraint지정
+///    aView.leadingAnchor.constriant(...).isActive = true
+///  }
+///}
+/// ```
+///
 protocol LayoutSupport {
   /// Set subviews constraints in root view
   func setConstraints()
 }
 
 extension LayoutSupport where Self: UIView {
+  // 함수 호출시 superView에 포함될 subviews 포함
   func setupUI(with subviews: UIView...) {
     addSubviews(subviews)
     setConstraints()
