d0c2c5fe0a30c35d2d0fded0fd77c8cc47db5dcc
shcommit <happysh_s2@naver.com>
2023-07-24 12:53:21 +0900

    [Feat] UISpacing 추가

--- MyKeynote/Source/Common/Model/UISpacing.swift
+++ MyKeynote/Source/Common/Model/UISpacing.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct UISpacing {
+  let top: CGFloat
+  let leading: CGFloat
+  let trailing: CGFloat
+  let bottom: CGFloat
+  
+  init(
+    leading: CGFloat = 0,
+    top: CGFloat = 0,
+    trailing: CGFloat = 0,
+    bottom: CGFloat = 0
+  ) {
+    self.top = top
+    self.leading = leading
+    self.trailing = trailing
+    self.bottom = bottom
+  }
+}
+
