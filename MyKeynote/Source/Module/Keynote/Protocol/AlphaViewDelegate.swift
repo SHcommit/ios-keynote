4ce5899f43dad89530e393e9394a605bd810aacb
shcommit <happysh_s2@naver.com>
2023-07-24 12:57:26 +0900

    [Feat] AlphaViewDelegate, KeynoteSlideMenuDataSource 추가!!

---
+++ MyKeynote/Source/Module/Keynote/Protocol/AlphaViewDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  AlphaViewDelegate.swift
+//  MyKeynote
+//
+//  Created by 양승현 on 2023/07/24.
+//
+
+protocol AlphaViewDelegate: AnyObject {
+  func valueChanged(_ value: Int)
+}
--- MyKeynote/Source/Module/Keynote/Protocol/KeynoteSlideMenuDataSource.swift
+++ MyKeynote/Source/Module/Keynote/Protocol/KeynoteSlideMenuDataSource.swift
@@ -5,7 +5,7 @@
 //  Created by 양승현 on 2023/07/21.
 //
 
-protocol KeynoteSlideMenuDataSource {
+protocol KeynoteSlideMenuDataSource: AnyObject {
   var numberOfItems: Int { get }
   func cellItem(at index: Int) -> SlideModel
 }
