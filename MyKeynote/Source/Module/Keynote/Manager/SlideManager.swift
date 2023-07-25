01c8f1ecf96cb47e35a616f581091339c5001380
shcommit <happysh_s2@naver.com>
2023-07-25 12:37:07 +0900

    [Feat] KeynoteViewController 가 UI render 해야 할 state 추가

--- MyKeynote/Source/Module/Keynote/Manager/SlideManager.swift
+++ MyKeynote/Source/Module/Keynote/Manager/SlideManager.swift
@@ -8,7 +8,6 @@
 import Foundation
 import Combine
 
-
 final class SlideManager {
   // MARK: - Properties
   private let rectModelFactory: RectModelAbstractFactory
@@ -18,14 +17,49 @@ final class SlideManager {
   
   private var slideModels: [SlideModel] = []
   
+  private var alphaValueSubject = PassthroughSubject<Int, Never>()
+  
+  private var subscription = Set<AnyCancellable>()
+  
   // MARK: - Lifecycle
   init(rectModelFactory: RectModelAbstractFactory) {
     self.rectModelFactory = rectModelFactory
+    bind()
+  }
+  
+  deinit {
+    NotificationCenter.default.removeObserver(
+      self,
+      name: .AlphaViewStepperValueChanged,
+      object: nil)
   }
 }
 
+// MARK: - @Objc Helper
+extension SlideManager {
+  @objc func setBgStateView(_ notification: Notification) {
+    let userInfoKey = AlphaView.Constant.Stepper.notificatonCenterPostKey
+    guard
+      let userInfo = notification.userInfo,
+      let alphaValue = userInfo[userInfoKey] as? Int
+    else {
+      return
+    }
+    alphaValueSubject.send(alphaValue)
+  }
+}
+
+
 // MARK: - Private helper
 private extension SlideManager {
+  func bind() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(setBgStateView),
+      name: .AlphaViewStepperValueChanged,
+      object: nil)
+  }
+  
   func makeRectModel() -> RectModel {
     let uniqueId = uniqueIdStorage.create(
       from: rectModelFactory)
@@ -50,7 +84,29 @@ private extension SlideManager {
 // MARK: - SlideManagerType
 extension SlideManager: SlideManagerType {
   func transform(input: Input) -> Output {
-    return input.appear.map {_ -> State in return .none }.eraseToAnyPublisher()
+    return Publishers
+      .MergeMany([
+        changedStepperValueChains(),
+        appearChains(with: input)])
+      .eraseToAnyPublisher()
+  }
+}
+
+// MARK: - Private helper
+private extension SlideManager {
+  func changedStepperValueChains() -> Output {
+    return alphaValueSubject
+      .subscribe(on: RunLoop.main)
+      .map { alpha -> State in
+        let alphaMaxValue = Double(AppSetting.UIConstAlpha.maxAlpha)
+        return .updateRectAlpha(Double(alpha)/alphaMaxValue)
+      }.eraseToAnyPublisher()
+  }
+  
+  func appearChains(with input: Input) -> Output {
+    return input.appear
+      .map { return .none }
+      .eraseToAnyPublisher()
   }
 }
 
