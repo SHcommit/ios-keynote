678cddf6452760b472abef4c18f46ce6e60fc17e
shcommit <happysh_s2@naver.com>
2023-07-26 02:27:06 +0900

    [Feat] statusBarHeight 추가

--- MyKeynote/Source/Common/Extension/UIView+.swift
+++ MyKeynote/Source/Common/Extension/UIView+.swift
@@ -11,4 +11,18 @@ extension UIView {
   func addSubviews(_ subviews: [UIView]) {
     _=subviews.map { addSubview($0) }
   }
+  
+  static var statusBarHeight: CGFloat {
+    if #available(iOS 13.0, *) {
+      guard
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+        let window = windowScene.windows.first
+      else {
+        return 0.0
+      }
+      return window.safeAreaInsets.top
+    } else {
+      return UIApplication.shared.statusBarFrame.size.height
+    }
+  }
 }
