66fc468a6fb1b233fef5af7416171bc6b34dc9d4
shcommit <happysh_s2@naver.com>
2023-07-25 13:46:04 +0900

    [Style] Slide detail view cell파일 생성 및 관련 코드 directory structure 변경

---
+++ MyKeynote/Source/Module/Keynote/View/SlideDetailView/SlideDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  SlideDetailView.swift
+//  MyKeynote
+//
+//  Created by 양승현 on 2023/07/25.
+//
+
+import Foundation
---
+++ MyKeynote/Source/Module/Keynote/View/SlideDetailView/SlideDetailViewCell/SlideDetailViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SlideDetailViewCell.swift
+//  MyKeynote
+//
+//  Created by 양승현 on 2023/07/25.
+//
+
+import Foundation
