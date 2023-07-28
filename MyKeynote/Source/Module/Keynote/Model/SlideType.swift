f8ff0dcf708fb061a2937acc0915f53f84824826
shcommit <happysh_s2@naver.com>
2023-07-28 10:22:13 +0900

    [Style] 이름 변경

--- MyKeynote/Source/Module/Keynote/Model/SlideType.swift
+++ MyKeynote/Source/Module/Keynote/Model/SlideType.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-struct SlideModel {
+/*
+ 헉... 대박.. 얕은복사....... RectModel은 클래스타입으로 했는데,
+ SlideModel은 struct로했고,, SlideManager's KeynoteViewAdapterDataSource로 특정 SlideModel을 반환할 때,,,
+ 얕은 복사가 되는 것으로 추정.. 그래서 rectModel의 특정 set함수로 데이터를 바꿔도 반영이 안되는 거였다..
+ */
+class SlideType {
   enum State {
     case rect (RectModel)
     case image(String)
