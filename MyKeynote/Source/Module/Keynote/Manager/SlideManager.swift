53815797e5c1f9c465d3ff76e50c9cffe7fcd641
shcommit <happysh_s2@naver.com>
2023-07-28 10:35:15 +0900

    [Feat] SlideManager관련 SlideRectModelAccessable, SlideAccessable, KeynoteViewAdapterDataSource, KeynoteSlideMenuDataSource 준수

--- MyKeynote/Source/Module/Keynote/Manager/SlideManager.swift
+++ MyKeynote/Source/Module/Keynote/Manager/SlideManager.swift
@@ -8,12 +8,6 @@
 import Foundation
 import Combine
 
-protocol SlideModelAccessable {
-  func add(_ slideModel: SlideModel)
-  
-  func update(_ slideModel: SlideModel, indexPath: IndexPath)
-}
-
 final class SlideManager {
   // MARK: - Properties
   private let rectModelFactory: RectModelAbstractFactory
@@ -21,7 +15,7 @@ final class SlideManager {
   private let uniqueIdStorage = UniqueIdRandomGeneratorStorage
     .shared
   
-  private var slideModels: [SlideModel] = []
+  private var slideModels: [SlideType] = []
   
   private var alphaValueSubject = PassthroughSubject<Int, Never>()
   
@@ -47,14 +41,15 @@ extension SlideManager {
     let userInfoKey = AlphaView.Constant.Stepper.notificatonCenterPostKey
     guard
       let userInfo = notification.userInfo,
-      let (alpha, indexPath) = userInfo[userInfoKey] as? (alpha: Int, indexPath: IndexPath),
-      let rectModel = slideModels[indexPath.row].getinstance as? RectModel
+      let (alpha, uniqueId) = userInfo[userInfoKey] as? (alpha: Int, uniqueId: String),
+      let rectModel = searchRectModel(with: uniqueId),
+      let rectModelIdx = findRectModelIndex(with: uniqueId)
     else {
+      print("nonononono T_T")
       return
     }
     rectModel.setAlpha(with: UInt8(alpha))
-    print("DEBUG: Update rectAlpha: \(rectModel.alpha)")
-    print(slideModels)
+    update(.init(rectModel: rectModel), at: rectModelIdx)
   }
 }
 
@@ -65,7 +60,6 @@ extension SlideManager {
     let uniqueId = uniqueIdStorage.create(
       from: rectModelFactory)
     let rgb = rectModelFactory.makeRGBRandomValue()
-    // let alpha = rectModelFactory.makeAlphaRandomValue()
     let alpha: UInt8 = 10
     
     let alphaModel = AlphaModel(alpha: alpha)
@@ -116,32 +110,62 @@ extension SlideManager: KeynoteSlideMenuDataSource  {
     slideModels.count
   }
   
-  func cellItem(at index: Int) -> SlideModel {
+  func cellItem(at index: Int) -> SlideType {
     return slideModels[index]
   }
 }
 
+// MARK: - KeynoteViewAdapterDataSource
 extension SlideManager: KeynoteViewAdapterDataSource {
-  func slideDetailViewCellItem(at index: Int) -> SlideModel {
+  func slideDetailViewCellItem(at index: Int) -> SlideType {
     return slideModels[index]
   }
   
-  func SlideMenuViewCellItem(at index: Int) -> SlideMenuView.slideMenuViewCellTypes {
+  func SlideMenuViewCellItem(
+    at index: Int
+  ) -> SlideMenuView.slideMenuViewCellTypes {
     switch slideModels[index].state {
-    case .image(_):
-      return .image
-    case .rect(_):
-      return .rect
+    case .image(_): return .image
+    case .rect(_): return .rect
     }
   }
 }
 
-extension SlideManager: SlideModelAccessable {
-  func add(_ slideModel: SlideModel) {
+// MARK: - SlideModelAccessable
+extension SlideManager: SlideAccessable {
+  func remove(at index: Int) -> SlideType? {
+    return slideModels.remove(at: index)
+  }
+  
+  func add(_ slideModel: SlideType) {
     slideModels.append(slideModel)
   }
   
-  func update(_ slideModel: SlideModel, indexPath: IndexPath) {
-    slideModels[indexPath.row] = slideModel
+  func update(_ slideModel: SlideType, at index: Int) {
+    slideModels[index] = slideModel
+  }
+}
+
+// MARK: - SlideRectModelAccessable
+extension SlideManager: SlideRectModelAccessable {
+  func searchRectModel(with uniqueId: String) -> RectModel? {
+    return slideModels
+      .first(where: {
+        guard let rectModel = $0.state.getRectModel else {
+          return false
+        }
+        return rectModel.uniqueID == uniqueId
+      })
+      .flatMap { $0.state.getRectModel }
+  }
+    
+  func findRectModelIndex(with uniqueId: String) -> Int? {
+    return slideModels
+      .firstIndex(where: {
+        guard let rectModel = $0.state.getRectModel else {
+          return false
+        }
+        return rectModel.uniqueID == uniqueId
+      })
   }
 }
